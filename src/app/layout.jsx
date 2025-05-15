3c6d852e8443a948f1ec21136c33286f95d65395
fiivxyxxng <scx200800@gmail.com>
2025-05-15 23:41:29 +0900

    feat: GNB 컴포넌트 구현

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,22 +11,28 @@ const pretendard = localFont({
 });
 
 const quanticoRegular = localFont({
-  src: '../assets/font/QuanticoRegular.ttf',
-  display: 'swap',
-  weight: '400',
-  variable: '--font-quantico-regular',
+  src: "../assets/font/QuanticoRegular.ttf",
+  display: "swap",
+  weight: "400",
+  variable: "--font-quantico-regular",
 });
 
 const quanticoBold = localFont({
-  src: '../assets/font/QuanticoBold.ttf',
-  display: 'swap',
-  weight: '700',
-  variable: '--font-quantico-bold',
+  src: "../assets/font/QuanticoBold.ttf",
+  display: "swap",
+  weight: "700",
+  variable: "--font-quantico-bold",
 });
+
+export const metadata = {
+  title: "Docthur - 독스루",
+  description: "개발문서 번역 플랫폼",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <link rel="icon" href="/favicon.svg" />
       <body
         className={`${pretendard.variable} font-pretendard flex min-h-screen flex-col antialiased`}
       >
