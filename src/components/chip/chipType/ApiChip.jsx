4a0d5ae6420455bf10d7039517685e6a422102f9
rakaso598 <112613372+rakaso598@users.noreply.github.com>
2025-05-16 13:12:37 +0900

    refactor: 'align-center'를 'items-center'로 변경 및 칩 컴포넌트 글자 높이 조정

--- src/components/chip/chipType/ApiChip.jsx
+++ src/components/chip/chipType/ApiChip.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ApiChip() {
   return (
-    <div className='flex align-center justify-center w-[47px] h-[26px] bg-[#FF905E] rounded-[8px] pt-[3px] pr-[12px] pb-[3px] pl-[12px] gap-[10px]'>
+    <div className='flex itmes-center justify-center w-[47px] h-[26px] bg-[#FF905E] rounded-[8px] pt-[3px] pr-[12px] pb-[3px] pl-[12px] gap-[10px]'>
       <span className='font-quantico-bold text-[#525252] font-bold text-[14px] leading-5'>API</span>
     </div>
   )
--- src/components/chip/chipType/ModernjsChip.jsx
+++ src/components/chip/chipType/ModernjsChip.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ModernjsChip() {
   return (
-    <div className='flex align-center justify-center w-[98px] h-[26px] bg-[#F66E6B] rounded-[8px] pt-[3px] pr-[12px] pb-[3px] pl-[12px] gap-[10px]'>
+    <div className='flex itmes-center justify-center w-[98px] h-[26px] bg-[#F66E6B] rounded-[8px] pt-[3px] pr-[12px] pb-[3px] pl-[12px] gap-[10px]'>
       <span className='font-quantico-bold text-[#525252] font-bold text-[14px] leading-5'>Modern JS</span>
     </div>
   )
