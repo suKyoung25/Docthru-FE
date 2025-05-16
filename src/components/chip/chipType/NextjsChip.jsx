12b6ef64ed76118008e22e0fa8180f9975c3195f
조성빈 <joseongbin@joseongbin-ui-MacBookAir.local>
2025-05-16 16:07:26 +0900

    feat:card component

--- src/components/chip/chipType/NextjsChip.jsx
+++ src/components/chip/chipType/NextjsChip.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function NextjsChip() {
   return (
-    <div className='flex itmes-center justify-center w-[73px] h-[26px] bg-[#79E16A] rounded-[8px] pt-[3px] pr-[12px] pb-[3px] pl-[12px] gap-[10px]'>
-      <span className='font-quantico-bold text-[#525252] font-bold text-[14px] leading-4.5'>Next.js</span>
+    <div className="itmes-center flex h-[26px] w-[73px] justify-center gap-[10px] rounded-[8px] bg-[#79E16A] pt-[3px] pr-[12px] pb-[3px] pl-[12px]">
+      <span className="font-quantico-bold text-[14px] font-bold text-[#525252]">
+        Next.js
+      </span>
     </div>
-  )
+  );
 }
 
-export default NextjsChip
+export default NextjsChip;
