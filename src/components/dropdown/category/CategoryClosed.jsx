f70c2a1a40c45b94544e9b0367ffd24585eff2ff
rakaso598 <112613372+rakaso598@users.noreply.github.com>
2025-05-19 18:45:19 +0900

    feat: 드롭다운 컴포넌트 '취소하기' 추가

--- src/components/dropdown/category/CategoryClosed.jsx
+++ src/components/dropdown/category/CategoryClosed.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import icToggleDown from "../../assets/icon/ic_toggle_down.svg"
+import icToggleDown from "../../../assets/icon/ic_toggle_down.svg"
 import Image from "next/image";
 
 export default function CategoryClosed() {
-  return <div className="flex bg-white w-200 h-14 border-[#E5E5E5] border-1 items-center justify-between p-4">
+  return <div className="flex bg-white w-200 h-14 border-[#E5E5E5] border-1 items-center justify-between p-4 rounded">
     <span className="text-[#A3A3A3] text-[16px]">카테고리</span>
     <Image src={icToggleDown} alt="Toggle Down Icon" width={24} height={24} />
   </div>;
