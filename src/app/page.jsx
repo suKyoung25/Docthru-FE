d0509465be97b2fe3537a27d89c7d654ead69cef
Sue <sue@Sueui-MacBookPro.local>
2025-05-20 18:12:19 +0900

    import titleImage 영역 삭제

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,15 @@ import Container, {RootLandingPageContainer} from "@/components/container/PageCo
 import Gnb from "@/layout/_components/Gnb";
 import titleDoct from "@/assets/img/img_title_doct.svg"
 import Image from "next/image";
-import BtnText from "@/components/btn/text/BtnText";
+
 
 export default function Home() {
   // 랜딩에선 따로 헤더 넣어주기
   return (
     <Container maxWidth="w-full" padding="p-0 sm:p-0 lg:p-0" className="flex flex-col">
       <Gnb userRole="guest" />
-      <Container maxWidth="w-full" className="bg-brand-black flex flex-col items-center" >
+      <div>랜딩페이지</div>
+      {/* <Container maxWidth="w-full" className="bg-brand-black flex flex-col items-center" >
         <div className="w-[300px] flex flex-col items-center">
           <Image src={titleDoct} alt="docthru" />
           <div className="mt-3 text-white font-semibold text-xl text-center">
@@ -20,7 +21,7 @@ export default function Home() {
       </Container>
       <Container maxWidth="w-full" className="bg-brand-black" >
           
-      </Container>
+      </Container> */}
     </Container>
   );
 }
