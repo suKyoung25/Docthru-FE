d3cbd9cf64d7c81d8be07ba76456c52d79ad1cb7
suKyoung25 <s.kyoung5496@gmail.com>
2025-05-21 16:21:39 +0900

    챌린지 페이지 제작 중

--- src/app/(user)/challenges/page.jsx
+++ src/app/(user)/challenges/page.jsx
@@ -3,8 +3,10 @@ import ApplyChallenge from "./_components/ApplyChallenge";
 function page() {
   return (
     <>
-      <div>
-        챌린지 목록 <ApplyChallenge />
+      <div className="mx-[16px] mt-[16px] mb-[65px]">
+        <div className="font-pretendard flex flex-row items-center justify-between text-[20px] font-semibold">
+          챌린지 목록 <ApplyChallenge />
+        </div>
       </div>
     </>
   );
