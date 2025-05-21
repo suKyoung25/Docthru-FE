438464c7cb16b21c7e6a2abc5334bdd5f0a71b5f
suKyoung25 <s.kyoung5496@gmail.com>
2025-05-21 15:43:04 +0900

    create apply-challenge-button in challenges-page

--- src/app/(user)/challenges/page.jsx
+++ src/app/(user)/challenges/page.jsx
@@ -1,23 +1,11 @@
-import Container from "@/components/container/PageContainer";
-import CallengeContainerd from "./_components/challengeCard/CallengeContainer";
-import SearchInput from "@/components/input/SearchInput";
-import Profile from "@/components/dropDown/Profile";
+import ApplyChallenge from "./_components/ApplyChallenge";
 
 function page() {
   return (
     <>
-      <Container>
-        챌린지 컨테이너 (type="slim" or "" 가능)
-        <CallengeContainerd height={"h-[176px]"} type={""} />
-      </Container>
-      <Container>
-        챌린지 검색창 placeholder 텍스트 사이즈 변경 가능
-        <SearchInput text={"text-[14px]"} />
-      </Container>
-      <Container>
-        드롭다운 유저 프로필
-        <Profile />
-      </Container>
+      <div>
+        챌린지 목록 <ApplyChallenge />
+      </div>
     </>
   );
 }
