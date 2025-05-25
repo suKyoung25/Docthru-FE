6bd7cca4a29b75f8ec24ad45c99e5462dd6a838b
fiivxyxxng <scx200800@gmail.com>
2025-05-25 22:01:57 +0900

    feat: 어드민 신청 관리 페이지 구현

--- src/app/admin/management/page.jsx
+++ src/app/admin/management/page.jsx
@@ -1,7 +1,53 @@
-import Container from "@/components/container/PageContainer";
+'use client';
 
-const page = () => {
-  return <Container>관리자 매니징 페이지</Container>;
-};
+import AppliedChallenges from '@/app/(user)/challenges/_components/myChallenges/appliedChallenges/AppliedChallenges';
+import DropdownListLeftSmall from '@/components/dropDown/list/DropdownListLeftSmall';
+import SearchInput from '@/components/input/SearchInput';
+import Sort from '@/components/sort/Sort';
+import { columnSetting, ITEM_COUNT } from '@/constant/constant';
+import { getChallenges } from '@/lib/api/searchChallenges';
+import { useEffect, useState } from 'react';
 
-export default page;
+function AdminManagementPage() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [applications, setApplications] = useState();
+  const [totalCount, setTotalCount] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const pageSize = ITEM_COUNT.APPLICATION;
+
+  const handleClickSort = () => setIsDropdownOpen((prev) => !prev);
+
+  const getApplications = async () => {
+    const challenges = await getChallenges({ page, pageSize });
+    setApplications(challenges?.data);
+    setTotalCount(challenges?.totalCount);
+  };
+
+  useEffect(() => {
+    getApplications();
+  }, [page]);
+
+  return (
+    <>
+      <h1 className="text-xl font-semibold mt-[26px] md:mt-[34px] mb-[13px] md:mb-6">챌린지 신청 관리</h1>
+      <div className="mb-4 md:mb-6 grid grid-cols-[2fr_1fr] gap-3 md:grid-cols-[4fr_1fr] lg:grid-cols-[6fr_1fr]">
+        <SearchInput />
+        <div className="relative">
+          <Sort isAdminStatus={true} onClick={handleClickSort} />
+          <div className="absolute right-0 mt-2">{isDropdownOpen && <DropdownListLeftSmall />}</div>
+        </div>
+      </div>
+      <AppliedChallenges
+        columnSetting={columnSetting}
+        result={applications}
+        totalCount={totalCount}
+        page={page}
+        pageSize={pageSize}
+        onPageChange={(newPage) => setPage(newPage)}
+      />
+    </>
+  );
+}
+
+export default AdminManagementPage;
