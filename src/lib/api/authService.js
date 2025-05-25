cd69b7d813c979f2587728e65f4caea825d25732
rakaso598 <nexonaddress98@gmail.com>
2025-05-25 14:13:30 +0900

    feat: middleware.js 디버깅코드 추가

--- src/lib/api/authService.js
+++ src/lib/api/authService.js
@@ -3,8 +3,8 @@ import { defaultFetch, tokenFetch } from '@/lib/fetchClient';
 export const authService = {
   logout: () => {
     console.warn('Consider using logoutAction from Server Actions for clearing httpOnly cookies.');
-    document.cookie = 'accessToken=; path=/; max-age=0; SameSite=Strict; Secure';
-    document.cookie = 'refreshToken=; path=/; max-age=0; SameSite=Strict; Secure';
+    document.cookie = 'accessToken=; path=/; max-age=0; SameSite=None;';
+    document.cookie = 'refreshToken=; path=/; max-age=0; SameSite=None;';
     return { success: true };
   }
 };
