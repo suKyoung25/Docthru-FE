718780905b8ef89b45f779abe5b6120162f14931
Sue <sue@Sueui-MacBookPro.local>
2025-05-25 15:17:02 +0900

    임시저장

---
+++ src/app/(user)/challenges/_components/myChallenges/appliedChallenges/MapResultData.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React, { useState } from 'react'
+import ListRow from './ListRow'
+
+export default function MapResultData({ resultData, columnSetting, onClick}) {
+
+  return (
+    
+    <div>
+        {resultData.map((dataList, key) => (
+          <ListRow 
+            key={key} 
+            data={dataList} 
+            columnSetting={columnSetting} 
+            onClick={onClick} 
+          />
+          ))}
+    </div>
+  )
+}
--- src/app/(user)/challenges/mychallenges/page.jsx
+++ src/app/(user)/challenges/mychallenges/page.jsx
@@ -6,8 +6,6 @@ import ChallengeContainerd from "../_components/challengeCard/ChallengeContainer
 import SearchInput from "@/components/input/SearchInput";
 import Profile from "@/components/dropDown/Profile";
 import ListHead from '../_components/myChallenges/appliedChallenges/ListHead';
-import ListRow from '../_components/myChallenges/appliedChallenges/ListRow';
-import AdminStatusChip from '../_components/myChallenges/appliedChallenges/AdminStatusChip';
 import { appliedChallenges } from '@/lib/api/myChallenges';
 import ApplyChallenge from '../_components/ApplyChallenge';
 import { useRouter } from 'next/navigation';
@@ -16,71 +14,57 @@ import FilterModal from '@/components/modal/FilterModal';
 import Sort from '@/components/sort/Sort';
 import { useAuth } from "@/providers/AuthProvider";
 import { getUserAction } from "@/lib/actions/auth";
+import MapResultData from "../_components/myChallenges/appliedChallenges/MapResultData";
+import { columnSetting } from "@/constant/constant";
+import { appliedMockData } from "./appliedMockData";
+import Pagination from "@/components/pagination/Pagination";
 
 
-/* 
-key : 테이블 필드명
-label : 필드의 게시판 타이틀명
-flex : 너비 비율
-className : 각 셀의 스타일링
-render : 해당 셀에 상태에 따라 다른 값을 노출해야 할 경우 
-*/
 
-const columnSetting = [
-    { key: "id", label: "No.", flex: 0.8 , className: "pl-4" },
-    { key: "docType", label: "분야", flex: 1 },
-    { key: "category", label: "카테고리", flex: 1 },
-    { key: "title", label: "챌린지 제목", flex: 5, className: "text-gray-700 font-medium"},
-    { key: "maxParticipant", label: "인원", flex: 1 },
-    { key: "createdAt", label: "신청일", flex: 1 },
-    { key: "updatedAt", label: "마감기한", flex: 1 },
-    { 
-      key: "adminSatus", 
-      label: "상태", 
-      flex: 1.2, 
-      render: 
-        (data) => 
-          !!data.application?.adminStatus 
-          // adminStatus 필드를 사용할 경우 아래 컴포넌트 사용해 값마다 다른 스타일링을 보여줌
-          ? (<AdminStatusChip status={data.application?.adminStatus}/> ) 
-          : "null" 
-    }
-]
+
 export default function page() {
   const router = useRouter();
-  const [result, setResult] = useState([]);
-  const { user, isLoading } = useAuth();
-  
-  if(isLoading) return (<div>로딩 중..</div>);
- 
-  console.log('User status changed!!:', user);
+  const [totalCount, setTotalCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const pageSize = 5; // 수정
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedData = appliedMockData.slice(startIndex, endIndex);
 
-  async function appliedChallengesData() {
-    try {
-      const data = await appliedChallenges();
-      setResult(data);
-    } catch (error) {
-      console.error("목록 불러오기 실패");
-    }
-  }
-  if (isLoading) return <div>로딩 중...</div>;
-  if (!user) return <div>로그인이 필요합니다.</div>;
-  console.log(user);
+
+  // const [result, setResult] = useState([]);
+  // const { user, isLoading } = useAuth();
+  
+  // if(isLoading) return (<div>로딩 중..</div>);
+ 
+  // console.log('User status changed!!:', user);
+
+  // async function appliedChallengesData() {
+  //   try {
+  //     const data = await appliedChallenges();
+  //     setResult(data);
+  //   } catch (error) {
+  //     console.error("목록 불러오기 실패");
+  //   }
+  // }
+  // if (isLoading) return <div>로딩 중...</div>;
+  // if (!user) return <div>로그인이 필요합니다.</div>;
+  // console.log(user);
   // 신청한 챌린지 목록 불러오기
 
-  useEffect(()=>{
-    if (user) appliedChallengesData();
-  },[])
+  // useEffect(()=>{
+  //   setResult(appliedMockData);
+  //  // if (user) appliedChallengesData();
+  // },[])
 
 
   
   return (
     <Container>
       <div className="flex justify-between h-10 mt-4">
-        { user.email ?? 
-        (<h2 className="flex-1 text-xl font-semibold">
-          챌린지 
-        </h2>)}
+        <h2 className="flex-1 text-xl font-semibold">
+          나의 챌린지 
+        </h2>
         <ApplyChallenge />
       </div>
       <MyChallengesTab />
@@ -89,14 +73,19 @@ export default function page() {
         <div className="flex-3 sm:flex-2"><Sort /></div>
       </div>
       <ListHead columnSetting={columnSetting} />
-      {result.map((appliedChallenges, idx) => (
-        <ListRow 
-          key={idx} 
-          data={appliedChallenges} 
-          columnSetting={columnSetting} 
-          onClick={() => router.push(`/challenges/${appliedChallenges.id}`)} 
-        />
-      ))}
+      <MapResultData 
+        columnSetting={columnSetting} // 매칭 데이터, 너비, 스타일링 셋팅
+        resultData={paginatedData} // api response
+        onClick={() => router.push(`/challenges/${data.id}`)} // 상세페이지로 이동
+      />
+      <div className="mt-5">
+      <Pagination
+        totalCount={appliedMockData.length} // ← 50
+        currentPage={page}
+        pageSize={pageSize}
+        onPageChange={setPage}
+      />
+      </div>
     </Container>
   );
 }
