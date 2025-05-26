a23ac33e545b57ecb71ac36846fe18379fde022c
suKyoung25 <s.kyoung5496@gmail.com>
2025-05-26 19:18:17 +0900

    resolve vercel Error2

--- src/components/dropDown/category/CategoryItems.jsx
+++ src/components/dropDown/category/CategoryItems.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 export default function CategoryItems({ toggleType, onSelect }) {
   let categories;
 
@@ -24,26 +23,6 @@ export default function CategoryItems({ toggleType, onSelect }) {
           </div>
         );
       })}
-=======
-export default function CategoryItems() {
-  return (
-    <div className="flex h-14 flex-col items-center justify-between rounded-lg border border-[#E5E5E5] bg-white">
-      <div className="flex h-14 w-full flex-col items-center justify-between rounded-t-lg border border-[#E5E5E5] bg-white p-4">
-        <span className="text-[16px] text-[#A3A3A3]">Next.js</span>
-      </div>
-      <div className="flex h-14 w-full flex-col items-center justify-between border border-[#E5E5E5] bg-white p-4">
-        <span className="text-[16px] text-[#A3A3A3]">API</span>
-      </div>
-      <div className="flex h-14 w-full flex-col items-center justify-between border border-[#E5E5E5] bg-white p-4">
-        <span className="text-[16px] text-[#A3A3A3]">Career</span>
-      </div>
-      <div className="flex h-14 w-full flex-col items-center justify-between border border-[#E5E5E5] bg-white p-4">
-        <span className="text-[16px] text-[#A3A3A3]">Modern JS</span>
-      </div>
-      <div className="flex h-14 w-full flex-col items-center justify-between rounded-b-lg border border-[#E5E5E5] bg-white p-4">
-        <span className="text-[16px] text-[#A3A3A3]">Web</span>
-      </div>
->>>>>>> dev
     </div>
   );
 }
