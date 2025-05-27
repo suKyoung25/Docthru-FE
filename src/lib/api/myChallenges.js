1577d1e03e04a57844a7347f9e0e82db776fa7d9
Sue <sue@Sueui-MacBookPro.local>
2025-05-27 11:08:02 +0900

    신청한 챌린지 페이지네이션 제작

--- src/lib/api/myChallenges.js
+++ src/lib/api/myChallenges.js
@@ -1,23 +1,34 @@
 "use client"
 // 나의 챌린지 관련 URL
-const BASE_URL = "http://localhost:8080/users/me/challenges";
+const BASE_URL = "http://localhost:8080";
 
 
 // 신청한 챌린지
-export async function appliedChallenges() {
+export async function appliedChallenges({page=1, pageSize, category, docType, keyword }) {
     try {
-        const res = await fetch(`${BASE_URL}?myChallengeStatus=applied`,{
+        const res = await fetch(`${BASE_URL}/users/me/challenges?myChallengeStatus=applied&page=${page}&pageSize=${pageSize}`,{
             method: "GET",
             credentials: "include",
         });
 
         if (!res.ok) throw new Error("챌린지 목록을 가져올 수 없습니다.");
-
+        
         const data = await res.json();
-        return data;
+        console.log(data)
+        return {
+            data: data.data,
+            totalCount: data.totalCount,
+            currentPage: data.currentPage,
+            pageSize: data.pageSize,
+        }
     
     } catch (error) {
         console.error(error);
-    
+         return {
+            data: [],
+            totalCount: 0,
+            currentPage: page,
+            pageSize,
+        };
     }  
 }
