70423a08808883d57763ea0d763573722dc78a1b
suKyoung25 <s.kyoung5496@gmail.com>
2025-05-28 14:13:52 +0900

    초성으로 챌린지 목록 검색 구현 중

--- src/app/(user)/challenges/page.jsx
+++ src/app/(user)/challenges/page.jsx
@@ -6,10 +6,11 @@ import SearchInput from "@/components/input/SearchInput";
 import ChallengeCard from "@/components/card/Card";
 import FilterModal from "@/components/modal/FilterModal";
 import Pagination from "@/components/pagination/Pagination";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import useChallenges from "@/hooks/useChallengeList";
 import { useAuth } from "@/providers/AuthProvider";
 import { useRouter } from "next/navigation";
+import Hangul from "hangul-js";
 
 function Page() {
   const [isModal, setIsModal] = useState(false);
@@ -56,8 +57,40 @@ function Page() {
     setIsModal(false);
   };
 
+  //초성으로 검색하기 위한 함수 (검색어의 초성만 추출하여 문자열로 반환)
+  const getInitials = (text) => {
+    if (!text) return "";
+    return Hangul.d(text, true)
+      .map(([initial]) => initial)
+      .join("");
+  };
+
+  //이미 검색어가 초성이라면 getInitials 함수 적용되면 오류남
+  const isInitialsOnly = (text) => {
+    // 초성 범위 (ㄱ ~ ㅎ) 정규식
+    return /^[ㄱ-ㅎ]+$/.test(text);
+  };
+
+  //초성 검색어로 챌린지를 찾아 반환하는 함수
+  const filteredChallenges = useMemo(() => {
+    if (!keyword) return challenges;
+
+    const keywordInitials = isInitialsOnly(keyword) ? keyword : getInitials(keyword);
+
+    const includeKeywordChallenges = challenges.filter((challenge) => {
+      const titleInitials = getInitials(challenge.title);
+      return titleInitials.includes(keywordInitials);
+    });
+
+    //디버깅
+    console.log("keywordInitials", keywordInitials); //정상
+    console.log("includeKeywordChallenges", includeKeywordChallenges);
+
+    return includeKeywordChallenges;
+  }, [challenges, keyword]);
+
   return (
-    <div className="mx-[16px] [@media(min-width:1200px)]:mx-[462px] mt-[16px] mb-[65px]">
+    <div className="mx-[16px] mt-[16px] mb-[65px] [@media(min-width:1200px)]:mx-[462px]">
       <div className="font-pretendard flex flex-row items-center justify-between text-[20px] font-semibold">
         챌린지 목록 <ApplyChallenge />
       </div>
@@ -82,13 +115,13 @@ function Page() {
 
       <div className="flex flex-col gap-[24px] py-[24px]">
         {isLoading ? (
-          <div className="flex text-[var(--color-gray-500)] flex-col w-full h-full justify-center items-center text-[16px] font-medium font-pretendard">
+          <div className="font-pretendard flex h-full w-full flex-col items-center justify-center text-[16px] font-medium text-[var(--color-gray-500)]">
             챌린지 목록을 불러오는 중...
           </div>
         ) : error ? (
           <div className="text-red-500">{error}</div>
-        ) : challenges.length > 0 ? (
-          challenges.map((challenge) => (
+        ) : filteredChallenges.length > 0 ? (
+          filteredChallenges.map((challenge) => (
             <div key={challenge.id} onClick={() => handleClickCard(challenge.id)}>
               <ChallengeCard
                 title={challenge.title}
@@ -102,7 +135,7 @@ function Page() {
             </div>
           ))
         ) : (
-          <div className="flex text-[var(--color-gray-500)] flex-col w-full h-full justify-center items-center text-[16px] font-medium font-pretendard">
+          <div className="font-pretendard flex h-full w-full flex-col items-center justify-center text-[16px] font-medium text-[var(--color-gray-500)]">
             <div>아직 챌린지가 없어요.</div>
             <div>지금 바로 챌린지를 신청해보세요!</div>
           </div>
