e95bf1e64783e13f1e355b7c24b01c4608051256
fiivxyxxng <scx200800@gmail.com>
2025-06-02 10:14:48 +0900

    fix: 구글 로그인 오류 해결

--- src/components/btn/auth/GoogleLoginButton.jsx
+++ src/components/btn/auth/GoogleLoginButton.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React from "react";
 import googleIcon from "@/assets/icon/ic_google.svg";
 import { useRouter } from "next/navigation";
+import { BASE_URL } from "@/constant/constant";
 
 export function GoogleLoginButton() {
   const router = useRouter();
@@ -11,7 +12,7 @@ export function GoogleLoginButton() {
   return (
     <button
       className="flex h-12 w-[343px] items-center justify-center gap-2 rounded-xl border border-gray-200 bg-white md:w-[518px]"
-      onClick={() => router.push("http://localhost:8080/auth/google")}
+      onClick={() => router.push(`${BASE_URL}/auth/google`)}
     >
       <Image src={googleIcon} alt="구글 아이콘" width={28} height={28} />
       <p>Google로 시작하기</p>
